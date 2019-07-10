7d29aa8edb8dcefa58dca5d5365e759c27305bd9
Gurinderpal Kaur <C0736399@mylambton.ca>
2019-07-10 11:59:20 -0400

    Fifth commit

--- C0736399Assignment3/C0736399Assignment3/Program.cs
+++ C0736399Assignment3/C0736399Assignment3/Program.cs
@@ -82,6 +82,7 @@ namespace C0736399Assignment3
         
         
     }
-  }
+    
+}
 
 
---
+++ C0736399Assignment3/LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class LinkedList
+    {
+
+    }
+    class Node
+    {
+        public string studentName;
+        public int first;
+        public Node prev;
+
+    }
+}
