2513c09081165d6593fe7450892c3296d87f29f2
Gurinderpal Kaur <C0736399@mylambton.ca>
2019-07-10 08:58:56 -0400

    Second Commit

--- C0736399Assignment3/C0736399Assignment3/Program.cs
+++ C0736399Assignment3/C0736399Assignment3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace C0736399Assignment3
     {
         static void Main(string[] args)
         {
+            var tp = new TextProcessing();
+            tp.Run();
+        }
+
+    }
+    class TextProcessing
+    {
+        public void Run()
+        {
+            using (StreamReader file = new StreamReader("C:/Users/gurin_9drx78b/Assignment3/C0736399Assignment3/C0736399Assignment3/names.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
         }
     }
 }
